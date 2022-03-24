1e8f8e85b651e16826fe065c830798def284a5d5
Lotiang Nyathio <93643349+Lotiang@users.noreply.github.com>
2022-03-24 12:20:24 +0300

    :+1:

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// grab elements (1)
+var add = document.getElementById('.addBtn')
+
+
+// create the function (2)
+function newElement(){
+    var newElement = document.createElement('p')
+    let p = document.createElement('onclik', '.addBtn')
+    results = write something her
+}  
